4909c7e9e809e53fae26e50946ed99fbbae91dae
miracleukeje <mo.ukeje@gmail.com>
2022-09-11 12:33:52 -0700

    compiled

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+/**
+* main - print the script in the put function 
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return
+*/
+int main(void)
+{
+             puts("\"Programming is like building a multilingual puzzle")
+             return (0);
+}
